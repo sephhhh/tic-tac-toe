b0aa7a2fa86f039ed4e59723dee1e4e771caab38
sephhhh <stephenwu07curry@gmail.com>
2024-04-16 11:36:18 -0400

    game will know when there is a tie and which player won

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //(function() {
     function Player(playerMark) {
+        let win = null;
         function addMark(row, column) {
             if (Gameboard.gameboard[row][column] == null) {
                 Gameboard.gameboard[row][column] = this.playerMark;
                 Game.checkWin();
+                Game.checkTie();
             } else {
                 console.log('this spot is already taken');
             }
@@ -11,6 +13,7 @@
         return {
             playerMark,
             addMark,
+            win,
         }
     }
 
@@ -21,24 +24,25 @@
     const Game = {
         player1: Player(0),
         player2: Player(1),
+        tie: null,
         checkWin: function() {
             let board = Gameboard.gameboard;
             for (let i=0; i<3; i++) {
                 if (board[0][i] != null) {
                     if (board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
                         if (board[0][i] == 0) {
-                            console.log("player1 wins!");
+                            this.player1.win = true;
                         } else {
-                            console.log("player2 wins!");
+                            this.player2.win = true;
                         }
                     }
                 }
                 if (board[i][0] != null) {
                     if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
                         if (board[i][0] == 0) {
-                            console.log("player1 wins!");
+                            this.player1.win = true;
                         } else {
-                            console.log("player2 wins!");
+                            this.player2.win = true;
                         }
                     }
                 }
@@ -46,22 +50,27 @@
             if (board[0][0] != null) {
                 if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
                     if (board[0][0] == 0) {
-                        console.log("player1 wins!");
+                        this.player1.win = true;
                     } else {
-                        console.log("player2 wins!");
+                        this.player2.win = true;
                     }
                 }
             } 
             if (board[2][0] != null) {
                 if (board[2][0] == board[1][1] && board[2][0] == board[0][2]) {
                     if (board[2][0] == 0) {
-                        console.log("player1 wins!");
+                        this.player1.win = true;
                     } else {
-                        console.log("player2 wins!");
+                        this.player2.win = true;
                     }
                 }
+            }
+        },
+        checkTie: function() {
+            let gameboardTie = Gameboard.gameboard.find(mark => mark == null);
+             if (gameboardTie == undefined) {
+                this.tie = true;
             }
         }
     }
 //})();
-
